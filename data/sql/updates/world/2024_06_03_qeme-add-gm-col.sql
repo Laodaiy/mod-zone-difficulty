564107da81641d902478d539914864ebbb4d98a4
duangon <ioridjh2002@163.com>
2024-07-22 20:47:59 +0800

    修复sql错误

--- data/sql/updates/world/2024_06_03_qeme-add-gm-col.sql
+++ data/sql/updates/world/2024_06_03_qeme-add-gm-col.sql
@@ -1,3 +1,6 @@
 -- Column to flag log entry for GM/Dev players.
-ALTER TABLE zone_difficulty_encounter_logs
-ADD COLUMN IF NOT EXISTS IsDev TINYINT(3) NULL DEFAULT 0 COMMENT '';
+-- ALTER TABLE zone_difficulty_encounter_logs
+-- ADD COLUMN IF NOT EXISTS IsDev TINYINT NULL DEFAULT 0 COMMENT '';
+
+ALTER TABLE `acore_characters`.`zone_difficulty_encounter_logs` 
+ADD COLUMN `IsDev` tinyint NULL DEFAULT 0 AFTER `Mode`;
